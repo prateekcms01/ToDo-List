b67f3e7968dfec1a6caeb6bd613753c658e62540
prateekcms01 <142109305+prateekcms01@users.noreply.github.com>
2024-02-10 01:49:52 +0530

    Add files via upload

---
+++ Todo.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPenToSquare } from '@fortawesome/free-solid-svg-icons'
+import { faTrash } from '@fortawesome/free-solid-svg-icons'
+
+export const Todo = ({task, deleteTodo, editTodo, toggleComplete}) => {
+  return (
+    <div className="Todo">
+        <p className={`${task.completed ? "completed" : "incompleted"}`} onClick={() => toggleComplete(task.id)}>{task.task}</p>
+        <div>
+        <FontAwesomeIcon className="edit-icon" icon={faPenToSquare} onClick={() => editTodo(task.id)} />
+        <FontAwesomeIcon className="delete-icon" icon={faTrash} onClick={() => deleteTodo(task.id)} />
+
+        {/* <i className="fa-solid fa-trash-can icon-delete" 
+        onClick={e=>{
+            props.deleteItem(props.index)
+        }}></i> */}
+        </div>
+    </div>
+  )
+}
